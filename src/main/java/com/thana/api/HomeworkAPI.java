14a4001d4d1fe24cbf4da77095b1d106df05dc91
Th1Thanakorn <80148630+Th1Thanakorn@users.noreply.github.com>
2022-01-06 20:20:18 +0700

    Update HomeworkAPI.java
    Fix and add special features

--- src/main/java/com/thana/api/HomeworkAPI.java
+++ src/main/java/com/thana/api/HomeworkAPI.java
@@ -2,6 +2,7 @@ package com.thana.api;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.thana.api.utils.ChatColor;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
@@ -19,6 +20,8 @@ public class HomeworkAPI {
 
     public static final String API_URL = "https://app.fakejson.com/q/1P7FQdFq?token=CVuc-35tnqfJ1eARz2OWYg";
     public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    public static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
+
     private static final HashMap<Date, Homeworks.HomeworkElement> HOMEWORKS_HISTORY = new HashMap<>();
 
     public static void main(String[] args) throws IOException, ParseException {
@@ -26,11 +29,15 @@ public class HomeworkAPI {
         URL url = new URL(API_URL);
         String contents = IOUtils.toString(url, StandardCharsets.UTF_8);
         Homeworks homeworks = HomeworkAPI.GSON.fromJson(contents, Homeworks.class);
+        String lastUpdated = homeworks.getLastUpdated();
+        String apiDate = homeworks.getApiDate();
         for (Homeworks.HomeworkElement element : homeworks.getHomeworks()) {
-            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-            Date date = dateFormat.parse(element.getDate());
+            Date date = DATE_FORMAT.parse(element.getDate());
             HOMEWORKS_HISTORY.put(date, element);
         }
+        System.out.println(ChatColor.WHITE + "API Date: " + ChatColor.GREEN + APIHelper.getRelatedDate(apiDate) + ChatColor.RESET);
+        System.out.println(ChatColor.WHITE + "Last Updated: " + ChatColor.GREEN + APIHelper.getRelatedDate(lastUpdated) + ChatColor.RESET);
+        System.out.print("\n");
 
         // API Getter
         Scanner scanner = new Scanner(System.in);
@@ -45,8 +52,7 @@ public class HomeworkAPI {
     }
 
     public static Homeworks.HomeworkElement getHomeworks(String day) throws ParseException {
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        Date date = dateFormat.parse(day);
+        Date date = DATE_FORMAT.parse(day);
         for (Date homeworkDate : HOMEWORKS_HISTORY.keySet()) {
             if (date.getDate() == homeworkDate.getDate()) {
                 return HOMEWORKS_HISTORY.get(homeworkDate);
