c0503a9db6b35264c83bef8975a5d2c444dd793b
Th1Thanakorn <80148630+Th1Thanakorn@users.noreply.github.com>
2022-01-07 17:09:38 +0700

    Fixed simple date format

--- src/main/java/com/thana/api/APIHelper.java
+++ src/main/java/com/thana/api/APIHelper.java
@@ -1,5 +1,6 @@
 package com.thana.api;
 
+import com.thana.api.utils.DateUtils;
 import com.thana.api.utils.TimeUtils;
 
 import java.text.ParseException;
@@ -7,7 +8,7 @@ import java.text.ParseException;
 public class APIHelper {
 
     public static String getRelatedDate(String date) throws ParseException {
-        long time = HomeworkAPI.DATE_FORMAT.parse(date).getTime();
+        long time = DateUtils.DATE_FORMAT.parse(date).getTime();
         return TimeUtils.getRelativeTime(time);
     }
 }
