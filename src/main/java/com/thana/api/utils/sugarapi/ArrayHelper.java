cdf284c61cbbe50fef254b47454145b8920f5215
IHCXNT4_43UI\uSEr <thanakorn14064@gmail.com>
2022-01-11 21:51:11 +0700

    Fixed array indexing

--- src/main/java/com/thana/api/utils/sugarapi/ArrayHelper.java
+++ src/main/java/com/thana/api/utils/sugarapi/ArrayHelper.java
@@ -5,7 +5,7 @@ public class ArrayHelper {
 
     public static <T> T[] skip(T[] array, int index) {
         T[] copy = array.clone();
-        System.arraycopy(array, index, copy, 0, array.length - 1);
+        System.arraycopy(array, index, copy, 0, 0);
         return copy;
     }
 }
