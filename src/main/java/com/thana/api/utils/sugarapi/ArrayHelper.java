8c56825e11756b7fee013c36ba51c1f56b96cd9f
IHCXNT4_43UI\uSEr <thanakorn14064@gmail.com>
2022-01-11 23:23:21 +0700

    Fixed array indexing counter

--- src/main/java/com/thana/api/utils/sugarapi/ArrayHelper.java
+++ src/main/java/com/thana/api/utils/sugarapi/ArrayHelper.java
@@ -1,11 +1,24 @@
 package com.thana.api.utils.sugarapi;
 
-// Utilities from: HTMLChannel
+import java.util.Arrays;
+
 public class ArrayHelper {
 
+    /**
+     * Skip any arrays by clone the array and filter an array using index counter.
+     * Utilities By: HTML Channel.
+     *
+     * @param array Input of array.
+     * @param index Start counting and collecting index of array.
+     * @return Skipped array on the given index.
+     */
     public static <T> T[] skip(T[] array, int index) {
-        T[] copy = array.clone();
-        System.arraycopy(array, index, copy, 0, 0);
+        T[] copy = Arrays.copyOf(array, array.length - 1);
+        for (int i = 0; i < array.length; i++) {
+            if (i >= index) {
+                copy[i - index] = array[i];
+            }
+        }
         return copy;
     }
 }
